7b5457212449b34f40da9dad35d6dd5ef3809eb4
Alper Ipek <3denizotesi@gmail.com>
2015-01-08 17:18:08 +0200

    cron sample fix

--- pns_cron_sample.sh
+++ pns_cron_sample.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Check APNS errors
 
-export PYTHONPATH=$PYTHONPATH:/home/appuser/pns/
+export PYTHONPATH=$PYTHONPATH:/home/user/pns/
 export PNSCONF="/home/user/config.ini"
 
 /home/user/env/bin/python /home/user/pns/workers/apns_feedback_worker.py
